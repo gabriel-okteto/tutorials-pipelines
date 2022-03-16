689066412e4e14792e94333072648a07a8d5c549
gabriel <gabriel@okteto.com>
2022-03-16 11:33:04 +0100

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting go-world server...")
+	http.HandleFunc("/", goServer)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func goServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "404 ish")
+}
